f93a8b5720a9b57e603c9cbd030572b1b9f1be99
Gabriela Stancheva <gabriela.stancheva2002@gmail.com>
2024-12-18 12:42:11 +0200

    AboutView change

--- src/components/AboutView.vue
+++ src/components/AboutView.vue
@@ -3,8 +3,12 @@
   <div class="container mx-auto my-8 text-center">
     <h2 class="text-3xl font-bold text-blue-600 mb-4">About GoCheckThis</h2>
     <p class="text-gray-600 text-lg">
+      Nice To Meet You!
+      We are Desi and Gabi and we are friends who love to travel and explore foreign countries and cultures.
+      With a lot of passion and work we created this page to help people like you learn about different landmarks around the world.
       GoCheckThis helps you explore and track Europe's most iconic landmarks.
       Discover places, add your favorites, and plan your next adventure!
+      Enjoy!
     </p>
   </div>
 </template>
